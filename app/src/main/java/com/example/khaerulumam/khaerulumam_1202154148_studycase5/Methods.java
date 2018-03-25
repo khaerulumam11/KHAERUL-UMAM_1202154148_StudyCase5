6f744177fe0e4206694cc0202b8541d0337fca28
unknown <ukhaerul8@gmail.com>
2018-03-25 21:29:25 +0700

    push kedua

---
+++ app/src/main/java/com/example/khaerulumam/khaerulumam_1202154148_studycase5/Methods.java
@@ -0,0 +1,8 @@
+package com.example.khaerulumam.khaerulumam_1202154148_studycase5;
+
+/**
+ * Created by Umam on 3/23/2018.
+ */
+
+public class Methods {
+}
---
+++ app/src/main/java/com/example/khaerulumam/khaerulumam_1202154148_studycase5/SettingActivity.java
@@ -0,0 +1,13 @@
+package com.example.khaerulumam.khaerulumam_1202154148_studycase5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
